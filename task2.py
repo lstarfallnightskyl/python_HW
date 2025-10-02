d6d47b9a8ce7572c8b160d576466cc0a33beae36
lstarfallnightskyl <l.starfall.nightsky.l@gmail.com>
2025-10-02 03:57:04 +0300

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,15 @@
+#'hello world'
+print("hello world")
+#'hello|world'
+print("hello", "world", sep="|")
+#'hello world!'
+print("hello", "world", end="!")
+#'A,B,C'
+print("A", "B", "C", sep=",")
+#'Line1 Line2'
+print("Line1", end=" ")
+print("Line2")
+#'Pyrhon>is>fun'
+print("Python", "is", "fun", sep=">")
+#'one-two-three--'
+print("one", "two", "three", sep="-", end="--")
