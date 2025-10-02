efb5ea6e7cb2323050c104bcf3e1aa19d2aeb063
lstarfallnightskyl <l.starfall.nightsky.l@gmail.com>
2025-10-02 15:46:00 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,15 +1,15 @@
-#'hello world'
+# 'hello world'
 print("hello world")
-#'hello|world'
+# 'hello|world'
 print("hello", "world", sep="|")
-#'hello world!'
+# 'hello world!'
 print("hello", "world", end="!")
-#'A,B,C'
+# 'A,B,C'
 print("A", "B", "C", sep=",")
-#'Line1 Line2'
+# 'Line1 Line2'
 print("Line1", end=" ")
 print("Line2")
-#'Pyrhon>is>fun'
+# 'Pyrhon>is>fun'
 print("Python", "is", "fun", sep=">")
-#'one-two-three--'
+# 'one-two-three--'
 print("one", "two", "three", sep="-", end="--")
