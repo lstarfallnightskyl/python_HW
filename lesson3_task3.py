d4b07116cc2279df149e8786096924b462baecca
lstarfallnightskyl <l.starfall.nightsky.l@gmail.com>
2025-10-04 05:36:14 +0300

    Create lesson3_task3.py

---
+++ lesson3_task3.py
@@ -0,0 +1,9 @@
+a = 10
+b = 3
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Division:", a / b)
+print("Multiplication:", a * b)
+print("Exponent (Power):", a ** b)
+print("Modulus:", a % b)
+print("Floor division:", a // b)
